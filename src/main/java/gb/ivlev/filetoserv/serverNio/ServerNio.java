c19edb9b1f463e76f6bc6fa20c38f78062159c40
Maxivich <maxivlevich@gmail.com>
2023-03-11 16:31:34 +0300

    1

---
+++ src/main/java/gb/ivlev/filetoserv/serverNio/ServerNio.java
@@ -0,0 +1,54 @@
+package gb.ivlev.filetoserv.serverNio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+
+public class ServerNio {
+
+
+    public static void main(String[] args) {
+        int port = 8899;
+        try {
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.socket().bind(new InetSocketAddress(port));
+            serverSocketChannel.configureBlocking(false);
+
+            Selector selector = Selector.open();
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("Сервер стартовал на порту "+ port+". Ожидаем соединения...");
+            while (true){
+                selector.select();
+                for(SelectionKey event : selector.selectedKeys()){
+                    if(event.isValid()){
+                        try {
+                            if(event.isAcceptable()){
+                                SocketChannel socketChannel = serverSocketChannel.accept();
+                                socketChannel.configureBlocking(false);
+                                System.out.println("Пожключен  " + socketChannel.getRemoteAddress());
+                                socketChannel.register(selector,SelectionKey.OP_READ);
+                            } else if (event.isReadable()) {
+                                SocketChannel socketChannel = (SocketChannel) event.channel();
+                                handleReadable(socketChannel);
+                            }
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void handleReadable(SocketChannel socketChannel) {
+
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module gb.ivlev.filetoserv {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.rmi;
 
     opens gb.ivlev.filetoserv to javafx.fxml;
     exports gb.ivlev.filetoserv.Client;
